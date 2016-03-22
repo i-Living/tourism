4e01764f6cd4d8086bd4cda50d0ffe4fa9e19b59
Aface <aface13@gmail.com>
2016-03-22 17:03:43 +0500

    just commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,21 @@ class Item < ActiveRecord::Base
   belongs_to :firm
   belongs_to :color
   belongs_to :branch
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   def self.latest
     Item.order(:updated_at).last
+  end
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'line items exists')
+      return false
+    end
   end
 end
